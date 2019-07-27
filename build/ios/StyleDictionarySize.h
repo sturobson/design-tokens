578dd9910da2583cde8c7f0305985b91cfe59f22
Stu Robson <stu.robson@embl.de>
2019-07-27 13:49:16 +0100

    initial style dictionairy init

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Sat, 27 Jul 2019 12:47:31 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Sat, 27 Jul 2019 12:47:31 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
